fdcccfa86eb62804c360916c1e594b7b00b8795c
Evan <evan@gmail.com>
2024-02-23 18:17:56 -0700

    Ok now it is final

--- FilmWebApp_Evan_Neff/Models/Movies.cs
+++ FilmWebApp_Evan_Neff/Models/Movies.cs
@@ -8,29 +8,31 @@ namespace FilmWebApp_Evan_Neff.Models
     public class Movies
     {
         [Key]
-     
         public int MovieID {get; set; }
+
         [Required]
         public string Title { get; set; }
         
         [StringLength(25)]
         public string ? Notes { get; set; }
+
         [Required]
         [ForeignKey("CategoryID")]
         public int CategoryID { get; set; }
+
         [Required]
-        [Range(1800, 2100, ErrorMessage = "Please enter a valid year")]
+        [Range(1888, 2100, ErrorMessage = "Please enter a valid year")]
         public int Year { get; set; } = 0;
         
         public string ? Director { get; set; }
 
-
         public string ? Rating { get; set; }
 
-
         public string ? LentTo { get; set; }
+
         [Required]
         public bool Edited { get; set; }
+
         [Required]
         public int CopiedToPlex { get; set; }
 
